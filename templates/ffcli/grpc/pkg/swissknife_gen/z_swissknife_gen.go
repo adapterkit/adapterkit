a763db9414cc04e2899b511bf282665c40acd560
ismael FALL <ismael.fall@epitech.eu>
2022-11-18 16:01:28 +0100

    fix: lint templated file
    fix : lint template
    
    fix : regen files

--- templates/ffcli/grpc/pkg/swissknife_gen/z_swissknife_gen.go
+++ templates/ffcli/grpc/pkg/swissknife_gen/z_swissknife_gen.go
@@ -1,51 +1,48 @@
-package swissknife_gen
+package swissknife_gen //nolint
 
 import (
-    "context"
-    "log"
+	"context"
+	"log"
 
-    "google.golang.org/grpc"
+	swissknife "github.com/pmg-tools/adapterkit-module-swissknife"
+	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
-
-    _ "moul.io/adapterkit/pkg/lib"
-
-    swissknife "github.com/pmg-tools/adapterkit-module-swissknife"
 )
 
 func SvcConvHexa(input string) (*swissknife.ConvHexaRes, error) {
 	ctx := context.Background()
 
 	conn, err := grpc.DialContext(ctx, "127.0.0.1:9314", grpc.WithTransportCredentials(insecure.NewCredentials()))
-    if err != nil {
+	if err != nil {
 		log.Fatal(err)
 	}
-    defer conn.Close()
+	defer conn.Close()
 
-    client := swissknife.NewSwissknifeSvcClient(conn)
-    req := &swissknife.ConvHexaReq{Input: input}
-    res, err := client.ConvHexa(ctx, req)
-    if err != nil {
-        return nil, err
-    }
+	client := swissknife.NewSwissknifeSvcClient(conn)
+	req := &swissknife.ConvHexaReq{Input: input}
+	res, err := client.ConvHexa(ctx, req)
+	if err != nil {
+		return nil, err
+	}
 
-    return res, nil
+	return res, nil
 }
 
 func SvcConvBase64(input string) (*swissknife.ConvBase64Res, error) {
 	ctx := context.Background()
 
 	conn, err := grpc.DialContext(ctx, "127.0.0.1:9314", grpc.WithTransportCredentials(insecure.NewCredentials()))
-    if err != nil {
+	if err != nil {
 		log.Fatal(err)
 	}
-    defer conn.Close()
+	defer conn.Close()
 
-    client := swissknife.NewSwissknifeSvcClient(conn)
-    req := &swissknife.ConvBase64Req{Input: input}
-    res, err := client.ConvBase64(ctx, req)
-    if err != nil {
-        return nil, err
-    }
+	client := swissknife.NewSwissknifeSvcClient(conn)
+	req := &swissknife.ConvBase64Req{Input: input}
+	res, err := client.ConvBase64(ctx, req)
+	if err != nil {
+		return nil, err
+	}
 
-    return res, nil
+	return res, nil
 }
--- templates/ffcli/unix-cmd/cmd/swissknife/main.go
+++ templates/ffcli/unix-cmd/cmd/swissknife/main.go
@@ -1,17 +1,17 @@
 package main
 
 import (
-    "context"
-    "flag"
-    "fmt"
-    "log"
-    "os"
+	"context"
+	"flag"
+	"fmt"
+	"log"
+	"os"
 
-    "test/pkg/swissknife_gen"
+	"test/pkg/swissknife_gen"
 
-    "github.com/peterbourgon/ff/v3"
-    "github.com/peterbourgon/ff/v3/ffcli"
-    swissknife "github.com/pmg-tools/adapterkit-module-swissknife"
+	"github.com/peterbourgon/ff/v3"
+	"github.com/peterbourgon/ff/v3/ffcli"
+	swissknife "github.com/pmg-tools/adapterkit-module-swissknife"
 )
 
 func initSvc() swissknife.SwissknifeSvcServer {
@@ -19,69 +19,68 @@ func initSvc() swissknife.SwissknifeSvcServer {
 }
 
 func main() {
-    if err := swissknifeRun(os.Args[1:]); err != nil {
-        log.Fatal(err)
-    }
-    return
+	if err := swissknifeRun(os.Args[1:]); err != nil {
+		log.Fatal(err)
+	}
 }
 
 func swissknifeRun(args []string) error {
-    rootFlagSet := flag.NewFlagSet("swissknife", flag.ExitOnError)
+	rootFlagSet := flag.NewFlagSet("swissknife", flag.ExitOnError)
 
-    root := &ffcli.Command{
-        FlagSet:    rootFlagSet,
-        ShortUsage: "swissknife [flags] <command> [args...]",
-        Options:    []ff.Option{ff.WithEnvVarNoPrefix()},
-        Subcommands: []*ffcli.Command{
-            convHexa(),
-            convBase64(),
-        },
-        Exec: func(_ context.Context, _ []string) error {
-            return flag.ErrHelp
-        },
-    }
+	root := &ffcli.Command{
+		FlagSet:    rootFlagSet,
+		ShortUsage: "swissknife [flags] <command> [args...]",
+		Options:    []ff.Option{ff.WithEnvVarNoPrefix()},
+		Subcommands: []*ffcli.Command{
+			convHexa(),
+			convBase64(),
+		},
+		Exec: func(_ context.Context, _ []string) error {
+			return flag.ErrHelp
+		},
+	}
 
-    return root.ParseAndRun(context.Background(), args)
+	return root.ParseAndRun(context.Background(), args)
 }
 
 func convHexa() *ffcli.Command {
-    var input string
-    convHexaFlagSet := flag.NewFlagSet("convHexa", flag.ExitOnError)
-    convHexaFlagSet.StringVar(&input, "Input", "", "")
-    return &ffcli.Command{
-        Name:       "convHexa",
-        ShortUsage: "swissknife convHexa ${input}",
-        Options:    []ff.Option{ff.WithEnvVarNoPrefix()},
-        FlagSet:    convHexaFlagSet,
-        Exec: func(_ context.Context, _ []string) error {
-            result, err := swissknife_gen.SvcConvHexa(input, initSvc())
-            if err != nil {
-                return err
-            }
+	var input string
+	convHexaFlagSet := flag.NewFlagSet("convHexa", flag.ExitOnError)
+	convHexaFlagSet.StringVar(&input, "Input", "", "")
+	return &ffcli.Command{
+		Name:       "convHexa",
+		ShortUsage: "swissknife convHexa ${input}",
+		Options:    []ff.Option{ff.WithEnvVarNoPrefix()},
+		FlagSet:    convHexaFlagSet,
+		Exec: func(_ context.Context, _ []string) error {
+			result, err := swissknife_gen.SvcConvHexa(input, initSvc())
+			if err != nil {
+				return err
+			}
 
-            fmt.Println(result)
-            return nil
-        },
-    }
+			fmt.Println(result)
+			return nil
+		},
+	}
 }
 
 func convBase64() *ffcli.Command {
-    var input string
-    convBase64FlagSet := flag.NewFlagSet("convBase64", flag.ExitOnError)
-    convBase64FlagSet.StringVar(&input, "Input", "", "")
-    return &ffcli.Command{
-        Name:       "convBase64",
-        ShortUsage: "swissknife convBase64 ${input}",
-        Options:    []ff.Option{ff.WithEnvVarNoPrefix()},
-        FlagSet:    convBase64FlagSet,
-        Exec: func(_ context.Context, _ []string) error {
-            result, err := swissknife_gen.SvcConvBase64(input, initSvc())
-            if err != nil {
-                return err
-            }
+	var input string
+	convBase64FlagSet := flag.NewFlagSet("convBase64", flag.ExitOnError)
+	convBase64FlagSet.StringVar(&input, "Input", "", "")
+	return &ffcli.Command{
+		Name:       "convBase64",
+		ShortUsage: "swissknife convBase64 ${input}",
+		Options:    []ff.Option{ff.WithEnvVarNoPrefix()},
+		FlagSet:    convBase64FlagSet,
+		Exec: func(_ context.Context, _ []string) error {
+			result, err := swissknife_gen.SvcConvBase64(input, initSvc())
+			if err != nil {
+				return err
+			}
 
-            fmt.Println(result)
-            return nil
-        },
-    }
+			fmt.Println(result)
+			return nil
+		},
+	}
 }
